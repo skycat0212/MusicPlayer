f89fdb3152f5006f8f96dabef2a462b6302573a4
key5771 <ksj57715@hanmail.net>
2020-11-20 15:21:29 +0900

    Data Load

--- MusicPlayer/MusicPlayer/View/MusicViewController.swift
+++ MusicPlayer/MusicPlayer/View/MusicViewController.swift
@@ -8,22 +8,40 @@
 import UIKit
 
 class MusicViewController: UIViewController {
+    
+    private var singer = ""
+    private var album = ""
+    private var songTitle = ""
+    private var duration = 0
+    private var image = ""
+    private var file = ""
+    private var lyrics = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        dataLoad()
         // Do any additional setup after loading the view.
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    private func dataLoad() {
+        NetworkRequest.shared.request { (response: MusicModel) in
+            guard let singer = response.singer,
+                  let album = response.album,
+                  let songTitle = response.title,
+                  let duration = response.duration,
+                  let image = response.image,
+                  let file = response.file,
+                  let lyrics = response.lyrics else { return }
+            
+            self.singer = singer
+            self.album = album
+            self.songTitle = songTitle
+            self.duration = duration
+            self.image = image
+            self.file = file
+            self.lyrics = lyrics
+        }
     }
-    */
 
 }
