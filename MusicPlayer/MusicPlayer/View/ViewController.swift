03dce629d7cee9a4e4825dc461d4af4c4b4b78eb
key5771 <ksj57715@hanmail.net>
2020-11-20 13:44:07 +0900

    Create Project

---
+++ MusicPlayer/MusicPlayer/View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by 김기현 on 2020/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
