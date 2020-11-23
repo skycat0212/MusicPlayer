199f6cf55f24c74c01a26e70abff696d8ce34e1d
key5771 <ksj57715@hanmail.net>
2020-11-23 15:42:31 +0900

    slider 및 time Label 생성

--- MusicPlayer/MusicPlayer/View/MusicViewController.swift
+++ MusicPlayer/MusicPlayer/View/MusicViewController.swift
@@ -7,11 +7,17 @@
 
 import UIKit
 import SnapKit
+import AVFoundation
 
 class MusicViewController: UIViewController {
     @IBOutlet weak var songTitleLabel: UILabel!
     @IBOutlet weak var singerLabel: UILabel!
     @IBOutlet weak var albumImageView: UIImageView!
+    @IBOutlet weak var timeSlider: UISlider!
+    @IBOutlet weak var minLabel: UILabel!
+    @IBOutlet weak var maxLabel: UILabel!
+    
+    private var audioPlayer: AVAudioPlayer?
     
     private var singer = ""
     private var album = ""
@@ -31,9 +37,11 @@ class MusicViewController: UIViewController {
     }
     
     private func makeConstraint() {
-        self.view.backgroundColor = .black
-        songTitleLabel.textColor = .white
-        singerLabel.textColor = .white
+//        self.view.backgroundColor = .black
+//        songTitleLabel.textColor = .white
+//        singerLabel.textColor = .white
+//        minLabel.textColor = .white
+//        maxLabel.textColor = .white
         
         songTitleLabel.snp.makeConstraints { make in
             make.top.equalToSuperview().offset(50)
@@ -50,11 +58,28 @@ class MusicViewController: UIViewController {
             make.trailing.equalToSuperview().offset(-60)
             make.top.equalTo(singerLabel.snp.bottom).offset(-self.view.bounds.height / 2)
         }
+        
+        timeSlider.snp.makeConstraints { make in
+            make.bottom.equalToSuperview().offset(-200)
+            make.leading.equalToSuperview().offset(60)
+            make.trailing.equalToSuperview().offset(-60)
+        }
+        
+        minLabel.snp.makeConstraints { make in
+            make.top.equalTo(timeSlider).offset(50)
+            make.leading.equalToSuperview().offset(60)
+        }
+        
+        maxLabel.snp.makeConstraints { make in
+            make.top.equalTo(timeSlider).offset(50)
+            make.trailing.equalToSuperview().offset(-60)
+        }
     }
     
     private func dataLoad() {
-        NetworkRequest.shared.request { (response: MusicModel) in
-            guard let singer = response.singer,
+        NetworkRequest.shared.request { [weak self] (response: MusicModel) in
+            guard let self = self,
+                  let singer = response.singer,
                   let album = response.album,
                   let songTitle = response.title,
                   let duration = response.duration,
@@ -71,6 +96,29 @@ class MusicViewController: UIViewController {
             self.songTitleLabel.text = songTitle
             self.albumImageView.sd_setImage(with: URL(string: image), completed: nil)
             print("lyrics: \(self.lyricsArr)")
+            
+            self.timeSlider.minimumValue = Float(0)
+            self.timeSlider.maximumValue = Float(self.duration)
+            
+            let time = self.timeSlider.maximumValue
+            let minute = Int(time) / 60
+            let second = Int(time) % 60
+            
+            self.setTimeLabel(minute: minute, second: second)
+            
+        }
+    }
+    
+    private func setTimeLabel(minute: Int, second: Int) {
+        var components = DateComponents()
+        components.setValue(minute, for: .minute)
+        components.setValue(second, for: .second)
+        
+        if let date = Calendar.current.date(from: components) {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "mm:ss"
+            self.minLabel.text = "00:00"
+            self.maxLabel.text = "\(formatter.string(from: date))"
         }
     }
 
